703cf85bae4c718cb920b4647d3f280c28fcd7b9
Karrtik Gupta <karrtikschoolid11@gmail.com>
2024-12-23 19:38:36 +0530

    Footer links

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "../styles/Footer.css"
+import {Link} from "react-router-dom";
 
 function Footer() {
   return (
@@ -8,11 +9,11 @@ function Footer() {
         <div className='logo_box align_centre'><div className="logo_button">TradeMyTicket</div></div>
       
       <div className="footer_links ">
-        <p>How it Works?</p>
-        <p>Explore</p>
-        <p>sell</p>
-        <p>Contact us</p>
-        <p>FAQs</p>
+        <Link to='/HowItWorks'><p>How it Works?</p></Link>
+        <Link to='/Explore'><p>Explore</p></Link>
+        <Link to='/Seller1'><p>sell</p></Link>
+        <Link to='/ContactUs'><p>Contact us</p></Link>
+        {/* <p>FAQs</p> */}
       </div>
     </div>
   );
