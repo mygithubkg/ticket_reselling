fe43101948192acbf4d5e1ee8476ed5b6dce4f2b
Karrtik Gupta <karrtikschoolid11@gmail.com>
2024-12-30 23:13:53 +0530

    Added Backend for event page

--- frontend/src/Pages/Event_Page.jsx
+++ frontend/src/Pages/Event_Page.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import "../styles/Event_Page.css"; // Optional: Add styles if needed
 import { Link } from "react-router-dom";
+import Ticket_card from '../Components/listing/Ticket_card';
 
 export default function Event_Page() {
   const { id } = useParams();
@@ -54,40 +55,15 @@ export default function Event_Page() {
       <p className='Intro-line'>{event.event_bio}</p>
       <div className='Upper_section'>
         <img className="align-centre event-photo" src={event.photo} alt={event.event_name} />
-        <div className='Upper_details'>
-          <div className='Pricing-Detials'>
-            <h1 className='Price'> <strong>₹{event.selling_price}</strong></h1>
-            <p><strong>Face Value:</strong> ₹{event.face_value}</p>
-          </div>
-          <div className='buttons'>
-            <button className='Interested'>
-              <Link className="sell" to="/addevent_3">Sell</Link>
-            </button>
-            <button className='Interested'>Buy</button>
-          </div>
-          <div className='Seller Details'>
-            <h2>Seller Details</h2>
-            <p>Name: {event.seller_name}</p>
-            <p>Rating: {event.seller_rating}</p>
-          </div>
-        </div>
-      </div>
-
-      <div className='details-container'>
         <div className='Event-Deatils'>
-          <p><strong>Event Type:</strong> {event.event_type}</p>
-          <p><strong>Date & Time:</strong> {event.event_date_time}</p>
-          <p><strong>Location:</strong> {event.event_location}</p>
-          <p><strong>Description:</strong> {event.event_description}</p>
-        </div>
-        <div className='Ticket-Detials'>
-          <p><strong>Ticket Type:</strong> {event.ticket_type}</p>
-          <p><strong>Quantity:</strong> {event.quantity}</p>
-          <p><strong>Seating Info:</strong> {event.seating_info}</p>
-          <p><strong>Transferable:</strong> {event.transferability ? "Yes" : "No"}</p>
-          <p><strong>Ticket Format:</strong> {event.ticket_format}</p>
+            <p><strong>Event Type:</strong> {event.event_type}</p>
+            <p><strong>Date & Time:</strong> {event.event_date}  {event.event_time}</p>
+            <p><strong>Location:</strong> {event.event_location}</p>
+            <p><strong>Description:</strong> {event.event_bio}</p>
         </div>
       </div>
+      <Ticket_card />
     </div>
+
   );
 }
