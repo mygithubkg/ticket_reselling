6a1162af30fb4141c5f7288a103c7780ea9db1a3
Karrtik Gupta <karrtikschoolid11@gmail.com>
2025-01-11 23:27:21 +0530

    Add Webpack configuration and update dependencies for frontend

---
+++ frontend/webpack.confiq.js
@@ -0,0 +1,43 @@
+import path from "path";
+
+module.exports = {
+  entry: './src/index.js', // Adjust the entry point as needed
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      "zlib": require.resolve("browserify-zlib"),
+      "buffer": require.resolve("buffer/"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "crypto": require.resolve("crypto-browserify"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000,
+  },
+};
